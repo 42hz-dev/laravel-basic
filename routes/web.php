35a868da5447fa716730b3b5dbfd8447fb75e76b
42hz <42hz.dev@gmail.com>
2025-01-31 23:25:02 +0900

    make models && make migration

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    $jobs = Job::all();
+    return view('jobs', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/{id}', function (int $id) {
+    $job = Job::find($id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
