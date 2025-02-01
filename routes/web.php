8d2cf4fdf3466d163dac9bf8eedab23833c2a91e
42hz <42hz.dev@gmail.com>
2025-02-01 21:46:07 +0900

    edit controller group or reousrce

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+//Route::resource('jobs', JobController::class, [
+//    // 'except' => ['edit'] // edit route 제거
+//    //'only' => ['index', 'show', 'create'] // index, show, create route 만 사용
+//]);
+
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
+
 
 Route::view('/contact', 'contact');
