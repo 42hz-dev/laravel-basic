3e169e189c400b27987b7f9680c8e3ebcb98f626
42hz <42hz.dev@gmail.com>
2025-02-01 21:17:15 +0900

    make controller

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,20 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job {
-    public static function all() : array
+class Job extends Model {
+
+    use HasFactory;
+
+    protected $table = 'job_listings';
+
+    protected $guarded = [];
+
+    public function employer() : object
     {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$40,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$50,000',
-            ],
-        ];
+        return $this->belongsTo(Employer::class);
     }
 
-    public static function find(int $id) : array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] === $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,16 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-Route::get('/jobs', function () {
-    $jobs = Job::all();
-    return view('jobs', ['jobs' => $jobs]);
-});
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
-Route::get('/jobs/{id}', function (int $id) {
-    $job = Job::find($id);
-    return view('job', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
